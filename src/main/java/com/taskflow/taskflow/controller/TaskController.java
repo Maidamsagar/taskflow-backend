c4a4f1f900ae4cddd35f7016f5eb8f76b3f4fdb7
sagar <maidamsagar@gmail.com>
2026-01-14 16:53:08 +0000

    3: Exposed GET /tasks REST API with controller

---
+++ src/main/java/com/taskflow/taskflow/controller/TaskController.java
@@ -0,0 +1,23 @@
+package com.taskflow.taskflow.controller;
+
+import com.taskflow.taskflow.entity.Task;
+import com.taskflow.taskflow.service.TaskService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class TaskController {
+
+    private final TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @GetMapping("/tasks")
+    public List<Task> getAllTasks() {
+        return taskService.getAllTasks();
+    }
+}
